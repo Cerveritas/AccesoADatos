335e19f1966f2eb7007fa956e9aeabb27637579e
SergioCerveraJ <seceji@gmail.com>
2023-11-27 08:36:36 +0100

    27/11/23

--- src/ejercicioConBBDD/lecturaDeRegistros.java
+++ src/ejercicioConBBDD/lecturaDeRegistros.java
@@ -21,6 +21,9 @@ public class lecturaDeRegistros {
             System.out.println("1 - Establecer conexion con la BBDD");
             System.out.println("2 - Mostrar todos los registros de emp_no, oficio y departamento");
             System.out.println("3 - Motrar ademas el campo dnombre ");
+            System.out.println("4 - Insertar un nuevo registro");
+            System.out.println("5 - Actualizar un registro");
+            System.out.println("6 - Borrar un registro");
             System.out.println("Introduce un numero: ");
             System.out.println("------------------------------------------------");
             System.out.println(" ");
@@ -44,6 +47,18 @@ public class lecturaDeRegistros {
                     masRegistros();
                     break;
 
+                case 4:
+                    insertarRegistro();
+                    break;
+
+                case 5:
+                    actualizarRegistro();
+                    break;
+
+                case 6:
+                    borrarRegistro();
+                    break;
+
                 default:
                     System.out.println("El numero introducido no es correcto, intentelo de nuevo...");
 
@@ -55,6 +70,7 @@ public class lecturaDeRegistros {
 
 
 
+    // METODO PARA ESTABLECER CONEXION CON LA BASE DE DATOS
     private static void establecerConexion() throws SQLException {
         String url="jdbc:mysql://localhost:3306/";
         String user= "root";
@@ -68,6 +84,7 @@ public class lecturaDeRegistros {
         System.out.println("Conexion y Asignacion realizada correctamente");
     }
 
+    // METODO PARA MOSTRAR REGISTROS ESPECIFICOS
     private static void registrosIndicados() throws SQLException {
         // Crear una consulta SQL para seleccionar los registros
         String query = "SELECT emp_no, apellido, oficio FROM empleados";
@@ -82,12 +99,10 @@ public class lecturaDeRegistros {
         while (resultSet.next()) {
             System.out.println(resultSet.getInt("emp_no") + " " + resultSet.getString("apellido") + " " + resultSet.getString("oficio"));
         }
-
-
-
     }
 
 
+    // METODO PARA MOSTRAR UN REGISTRO DE MAS
     private static void masRegistros() throws SQLException {
 
         // Crear una consulta SQL para seleccionar los registros
@@ -104,6 +119,67 @@ public class lecturaDeRegistros {
         while (resultSet.next()) {
             System.out.println(resultSet.getInt("emp_no") + " " + resultSet.getString("apellido") + " " + resultSet.getString("oficio") + " " + resultSet.getString("dnombre"));
         }
+    }
+
+
+    // METODO PARA INSERTAR UNA NUEVA TUPLA, EN ESTE CASO EN LA TABLA DEPARTAMENTO
+    private static void insertarRegistro() throws SQLException {
+
+        PreparedStatement ps = conn.prepareStatement("insert into departamentos values (?,?,?)");
+
+        System.out.println("Introduce el dept_no (INT)");
+            int dept_no = sc.nextInt();
+        System.out.println("Introduce el nombre (STRING)");
+            String nombre = sc.next();
+        System.out.println("Introudce la localizacion (STRING)");
+            String loc = sc.next();
+
+        ps.setInt(1, dept_no);
+        ps.setString(2, nombre);
+        ps.setString(3, loc);
+
+        ps.executeUpdate();
+
+        System.out.println("Registro insertado correctamente");
+
+    }
+
+    // METODO PARA ACTUALIZAR UN REGISTRO DE LA TABLA DEPARTAMENTO
+    private static void actualizarRegistro() throws SQLException {
+
+        PreparedStatement ps = conn.prepareStatement("update departamentos set dnombre = ?, loc = ? where dept_no = ?");
+
+        System.out.println("Introduce el dept_no");
+            int dept_no = sc.nextInt();
+
+        //ACTUALIAMOS
+        System.out.println("Introduce el nombre");
+            String nombre = sc.next();
+        System.out.println("Introduce la localizacion");
+            String loca = sc.next();
+
+        ps.setString(1, nombre);
+        ps.setString(2, loca);
+        ps.setInt(3, dept_no);
+
+        ps.executeUpdate();
+
+        System.out.println("El registro con el DEPT_NO "+dept_no+" ha sido actualizado");
+
+    }
+    // METODO PARA BORRAR UN REGISTRO POR SU ID EN LA TABLA DEPARTAMENTO
+    private static void borrarRegistro() throws SQLException {
+
+        PreparedStatement ps = conn.prepareStatement("delete from departamentos where dept_no = ?");
+
+        System.out.println("Indicame el DEPT_NO que del registro que deseas eliminar");
+            int id = sc.nextInt();
+
+        ps.setInt(1, id);
+
+        ps.executeUpdate();
+
+        System.out.println("El registro con el DEPT_NO "+id+" ha sido eliminado correctamente...");
     }
 
 
